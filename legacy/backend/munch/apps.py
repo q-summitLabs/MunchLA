21345659981a9c348c2a5998629c0b81d5eba0d3
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-09-27 12:50:50 -0700

    Repository clean up

--- legacy/backend/munch/apps.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=E0401
-# pylint: disable=W0718
-
-from django.apps import AppConfig
-
-
-class MunchConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'munch'
