1395030a402cc3fe828e6c3d847d6f38416d94da
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-09-27 23:11:57 -0700

    Added configs

--- main/next.config.mjs
+++ main/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NEXT_PUBLIC_API_URL + "/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
