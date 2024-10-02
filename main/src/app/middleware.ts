cbe6d7efacde193c708b381ac2943e6e72b7c093
itsmandrew <89715746+itsmandrew@users.noreply.github.com>
2024-10-01 22:25:01 -0700

    Updating sidebar visuals + changed rate limit

--- main/src/app/middleware.ts
+++ main/src/app/middleware.ts
@@ -4,13 +4,13 @@ import { kv } from '@vercel/kv';
 
 const ratelimit = new Ratelimit({
     redis: kv,
-    limiter: Ratelimit.slidingWindow(3, '100 s'),
+    limiter: Ratelimit.slidingWindow(1000, '30 s'),
 });
 
 export default async function middleware(request: NextRequest) {
     const ip = request.ip ?? "127.0.0.1";
     const { success, pending, limit, reset, remaining } = await ratelimit.limit(
-    ip
+        ip
     );
     console.log(pending, limit, reset, remaining);
     return success;
